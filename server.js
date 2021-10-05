8222fff204cfa20b5d5d497eba94596176e021ae
FAFA <f.monazami@avakatan.com>
2021-10-05 19:01:07 +0330

    ⚡️minor update

--- server.js
+++ server.js
@@ -8,6 +8,33 @@ const responses = []
 
 // Middlewares
 
+const imageProcess = async function () {
+  await sleep(500)
+  const data = { someData: `data number ${Math.floor(Math.random() * 10000)}` }
+  responses.push(data)
+  return data
+}
+
+const processData = async function () {
+  const res = responses
+  await sleep(500)
+  const randomSuccessRate = Math.random()
+  if (randomSuccessRate < 0.1) {
+    //? Unsuccessful
+  } else {
+    //? return "success"
+  }
+}
+
+const sleep = (ms = 500) => new Promise(resolve => setTimeout(resolve, ms))
+
+const scheduler = function () {
+  cron.schedule("0 15 * * * *", () => {
+    console.log("Hello from the cron")
+    imageProcess()
+  })
+}
+
 // Route Middlewares
 
 // Routes
@@ -27,19 +54,3 @@ app.listen(4000, () => {
   console.log("Server is running")
   scheduler()
 })
-
-const imageProcess = async function () {
-  await sleep(500)
-  const data = { someData: `data number ${Math.floor(Math.random() * 10000)}` }
-  responses.push(data)
-  return data
-}
-
-const sleep = (ms = 500) => new Promise(resolve => setTimeout(resolve, ms))
-
-const scheduler = function () {
-  cron.schedule("0 15 * * * *", () => {
-    console.log("Hello from the cron")
-    imageProcess()
-  })
-}
